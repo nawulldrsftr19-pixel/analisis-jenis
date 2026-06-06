fdedc25f988272863351e31972c65136e0e7b002
nawulldrsftr19-pixel <nawulldrsftr19@gmail.com>
2026-06-06 11:25:25 +0700

    Update analisisjeniss.py

--- analisisjeniss.py
+++ analisisjeniss.py
@@ -69,8 +69,8 @@ with col_center:
     # --- TAB 1: BAGAN ALIR ---
     with tab1:
         st.subheader("📍 Mind Map Pemisahan Kation [1]")
-        try:
-            st.image![image alt](https://github.com/nawulldrsftr19-pixel/analisis-jenis/blob/8328186babe214c1fbff44c5e42a46837ab6dd65/bagan%20pemisahan%20kation.png)
+
+[image alt](https://github.com/nawulldrsftr19-pixel/analisis-jenis/blob/8328186babe214c1fbff44c5e42a46837ab6dd65/bagan%20pemisahan%20kation.png)
 
     # --- TAB 2: ANALISIS KATION ---
     with tab2:
