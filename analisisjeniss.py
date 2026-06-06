24ca7485dd00fd08125d7a165d01808bd5c8c7a5
nawulldrsftr19-pixel <nawulldrsftr19@gmail.com>
2026-06-06 14:06:43 +0700

    Update analisisjeniss.py

--- analisisjeniss.py
+++ analisisjeniss.py
@@ -185,9 +185,9 @@ with tab2:
     st.subheader("🛠️ Analisis Kation")
     gol = st.selectbox("Pilih Golongan:",["Golongan I","Golongan III","Golongan IV"])
     
- elif gol=="Golongan I":
-    st.info("Uji dengan HCl encer → endapan AgCl, PbCl₂, Hg₂Cl₂.")
-    if st.button("Jalankan Uji Gol I"):
+   elif gol=="Golongan I":
+        st.info("Uji dengan HCl encer → endapan AgCl, PbCl₂, Hg₂Cl₂.")
+        if st.button("Jalankan Uji Gol I"):
         # Reaksi kation Golongan I
         st.latex(r"Ag^+ + Cl^- \rightarrow AgCl(s) \downarrow \text{ (Putih)}")
         st.latex(r"Pb^{2+} + 2Cl^- \rightarrow PbCl_2(s) \downarrow \text{ (Putih)}")
