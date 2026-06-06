61a3650a87546099346705bdfb907a8e35d475c6
nawulldrsftr19-pixel <nawulldrsftr19@gmail.com>
2026-06-06 14:13:55 +0700

    Update analisisjeniss.py

--- analisisjeniss.py
+++ analisisjeniss.py
@@ -187,11 +187,12 @@ with tab2:
 
     if gol=="Golongan I":
         st.info("Uji dengan HCl encer → endapan AgCl, PbCl₂, Hg₂Cl₂.")
+        
         if st.button("Jalankan Uji Gol I"):
-        # Reaksi kation Golongan I
-        st.latex(r"Ag^+ + Cl^- \rightarrow AgCl(s) \downarrow \text{ (Putih)}")
-        st.latex(r"Pb^{2+} + 2Cl^- \rightarrow PbCl_2(s) \downarrow \text{ (Putih)}")
-        st.latex(r"Hg_2^{2+} + 2Cl^- \rightarrow Hg_2Cl_2(s) \downarrow \text{ (Putih)}")
+            # Reaksi kation Golongan I
+            st.latex(r"Ag^+ + Cl^- \rightarrow AgCl(s) \downarrow \text{ (Putih)}")
+            st.latex(r"Pb^{2+} + 2Cl^- \rightarrow PbCl_2(s) \downarrow \text{ (Putih)}")
+            st.latex(r"Hg_2^{2+} + 2Cl^- \rightarrow Hg_2Cl_2(s) \downarrow \text{ (Putih)}")
         
         # Animasi sentrifugasi
         centrifuge_action()
